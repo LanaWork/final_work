c3f31d05844c77cd7c4db4b7817e1291c67263b5
Lana <117645857+LanaWork@users.noreply.github.com>
2022-12-08 12:09:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. */
+string[] num1 = new string[4] {"hello", "2", "world", ":)"};
+string[] num2 = new string[num1.Length];
+void SecondArrayWithIF(string[] num1, string[] num2)
+{
+    int count = 0;
+    for (int i = 0; i < num1.Length; i++)
+    {
+    if(num1[i].Length <= 3)
+        {
+        num2[count] = num1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write($"{num[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(num1, num2);
+PrintArray(num2);
